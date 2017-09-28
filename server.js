49bd5b471f0ffdb3592cadeeedc233db830fa6cd
Rizkiyana Prima Putra <rizkiyanapp@gmail.com>
2017-09-28 22:58:24 +0700

    last commit

--- server.js
+++ server.js
@@ -3,19 +3,31 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var path = require('path');
+var twit = require('twit')
 
-// INIT STATIC FILES DIR
-// app.use(express.static(path.join(__dirname + '/static')));
+
+// INIT TWIT
+var T = new twit({
+  consumer_key:         '4yt2POfzwdG38v1oIZEPkjVBh',
+  consumer_secret:      '0epHSeAZHHHw6EG0a4SAOJVE2aenhBbkmyERLG3k8yPPcgsEXZ',
+  access_token:         '258342186-ZaMJQkrmJ5EkZc2z4Zu6KbiDBmqSwLxTo64ggFFC',
+  access_token_secret:  'Fk4ZsZpp1fYb23vga7jHuRo16zkIGdAZf8DFqkr4FGhGf',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+});
 
 // SERVER INIT
-// http.listen(process.env.PORT || 3000, function(){
-//   console.log('Server running at *:3000');
-// });
-
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+  console.log('Server running at *:3000')
+});
+
+// GET
+app.get('/', function (req, res) {
+  res.send('Hello World')
+});
+
+app.get('/search/:key', function(req, res) {
+  T.get('search/tweets', { q: req.params.key, count: 50 }, function(err, data, response) {
+    res.json(data)
+  })
+  // res.send(req.params.key)
+});
